e59cff3869f40e8c83a920219c055cf05382e258
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-11-15 22:55:44 +0000

    Set up component library build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts({ include: "src" }), libInjectCss()],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "src/soprano-ui.ts"),
+      name: "SopranoUI",
+      fileName: "soprano-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
