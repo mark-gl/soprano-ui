57c7e1d9774211305897c2ea1610e227ffb773fe
mark-gl <26033115+mark-gl@users.noreply.github.com>
2023-11-16 18:21:30 +0000

    Implement MediaSlider styles

--- src/media-slider/MediaSlider.tsx
+++ src/media-slider/MediaSlider.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import * as RadixSlider from "./Slider";
 import { SliderProps } from "./Slider";
+import styles from "./MediaSlider.module.css";
 
 export const MediaSlider = React.forwardRef<HTMLDivElement, SliderProps>(
   (props, forwardedRef) => {
     return (
-      <RadixSlider.Root {...props} ref={forwardedRef}>
-        <RadixSlider.Track>
-          <RadixSlider.Range />
+      <RadixSlider.Root className={styles.slider} {...props} ref={forwardedRef}>
+        <RadixSlider.Track className={styles.track}>
+          <RadixSlider.Range className={styles.range} />
         </RadixSlider.Track>
-        <RadixSlider.Thumb />
+        <RadixSlider.Thumb className={styles.thumb} />
       </RadixSlider.Root>
     );
   }
