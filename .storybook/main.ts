0eadcfef3cf7a06852237db2eac7a82973033116
mark-gl <26033115+mark-gl@users.noreply.github.com>
2026-01-31 00:25:11 +0000

    Migrate to Storybook 10

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,18 +5,16 @@ const config: StorybookConfig = {
     "../stories/**/*.mdx",
     "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
+
   addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/addon-a11y",
+    "@storybook/addon-docs",
   ],
+
   framework: {
     name: "@storybook/react-vite",
     options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+  }
 };
 export default config;
--- stories/SectionTree.stories.tsx
+++ stories/SectionTree.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/react-vite";
 import { SectionTree } from "../src/section-tree/SectionTree";
 import { ExampleEnvironment } from "./example/ExampleEnvironment";
 
